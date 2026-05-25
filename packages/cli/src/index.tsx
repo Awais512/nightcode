d125842f60de10a7d3b93aa4d28cd69cfe36a9e7
awais512 <raza.awais25@gmail.com>
2026-05-25 23:06:04 +0100

    Added keyboard provider

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -4,26 +4,29 @@ import { Header } from "./components/header";
 import { StatusBar } from "./components/status-bar";
 import { InputBar } from "./components/input-bar";
 import { ToastProvider } from "./providers/toast";
+import { KeyboardLayerProvider } from "./providers/keyboard-layer";
 
 function App() {
   return (
-    <ToastProvider>
-      <box
-        alignItems="center"
-        justifyContent="center"
-        backgroundColor="#0D0D12"
-        width="100%"
-        height="100%"
-        gap={2}
-      >
-        <Header />
-        <box width="100%" maxWidth={78} paddingX={2}>
-          <InputBar onSubmit={() => {}} />
+    <KeyboardLayerProvider>
+      <ToastProvider>
+        <box
+          alignItems="center"
+          justifyContent="center"
+          backgroundColor="#0D0D12"
+          width="100%"
+          height="100%"
+          gap={2}
+        >
+          <Header />
+          <box width="100%" maxWidth={78} paddingX={2}>
+            <InputBar onSubmit={() => {}} />
+          </box>
         </box>
-      </box>
-    </ToastProvider>
+      </ToastProvider>
+    </KeyboardLayerProvider>
   );
 }
 
-const renderer = await createCliRenderer();
+const renderer = await createCliRenderer({ exitOnCtrlC: false });
 createRoot(renderer).render(<App />);
